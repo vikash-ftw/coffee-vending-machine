50f26b34c050fb54c1483cdf6ea3c6130cb40dfd
Vikash Batham <31383789+Vikash7TM@users.noreply.github.com>
2020-06-13 11:52:49 +0530

    Update CoffeeMachine.java

--- src/CoffeeMachine.java
+++ src/CoffeeMachine.java
@@ -1,10 +1,7 @@
 import java.util.Scanner;
-import java.lang.Math;
+
 public class CoffeeMachine {
     final static Scanner scn = new Scanner(System.in);
-//    final static int reqWater = 200;
-//    final static int reqMilk = 50;
-//    final static int reqBeans = 15;
 
     static int water,milk,beans,disCups;
     static int wallet;
@@ -121,47 +118,7 @@ public class CoffeeMachine {
     }
 
     public static void main(String[] args) {
-        //System.out.println("Hello World!");
-//        System.out.println("Starting to make a coffee\nGrinding coffee beans\nBoiling water\nMixing boiled water with crushed coffee beans");
-//        System.out.println("Pouring coffee into the cup\nPouring some milk into the cup\nCoffee is ready!");
-//
-//        int numCupsOfCofee = scn.nextInt();
-//        String req = String.format("%s\n%s\n%s\n%s",
-//                "For " + numCupsOfCofee + "cups of coffee you will need:",
-//                reqWater * numCupsOfCofee + " ml of water",
-//                reqMilk * numCupsOfCofee + " ml of milk",
-//                reqBeans * numCupsOfCofee + " g of coffee beans");
-//        System.out.println(req);
-//
-//        System.out.println("Write how many ml of water the coffee machine has:");
-//        int sWater = scn.nextInt();
-//        System.out.println("Write how many ml of milk the coffee machine has:");
-//        int sMilk = scn.nextInt();
-//        System.out.println("Write how many grams of coffee beans the coffee machine has:");
-//        int sBeans = scn.nextInt();
-//        System.out.println("Write how many cups of coffee you will need:");
-//        int coffeeAmount = scn.nextInt();
-//
-//        int rWater = sWater-(reqWater*coffeeAmount);
-//        int rMilk = sMilk-(reqMilk*coffeeAmount);
-//        int rBeans = sBeans-(reqBeans*coffeeAmount);
-//        if (rWater>-1 && rMilk>-1 && rBeans>-1) {
-//            System.out.print("Yes, I can make that amount of coffee");
-//            if(rWater>=reqWater && rMilk>=reqMilk && rBeans>=reqBeans) {
-//                int xtCups = makeCups(rWater,rMilk,rBeans);
-//                System.out.println(" (and even " + xtCups + " more that that)");
-//            }
-//        } else {
-//            System.out.print("No");
-//            if(sWater>=reqWater && sMilk>=reqMilk && sBeans>=reqBeans){
-//                int onlyCups = makeCups(sWater,sMilk,sBeans);
-//                System.out.println(", I can make only "+onlyCups+" cup(s) of coffee");
-//            }else {
-//                System.out.println(", I can make only 0 cup(s) of coffee");
-//            }
-//        }
-//        System.out.println();
-
+   
         getInitState(400,540,120,9,550);
         while (true) {
             System.out.println();
@@ -169,11 +126,5 @@ public class CoffeeMachine {
             System.out.println();
         }
     }
-//    public static int makeCups(int rWater,int rMilk,int rBeans){
-//        int xtWater = rWater / reqWater;
-//        int xtMilk = rMilk / reqMilk;
-//        int xtBeans = rBeans / reqBeans;
-//        return Math.min(Math.min(xtWater, xtMilk), Math.min(xtMilk, xtBeans));
-//    }
 
 }
